b91fd8f68f91544fa2e8405af500cd07790fe22e
shiv2929 <shivanshu.shrivastava@gmail.com>
2017-01-26 09:58:02 -0800

    updated

--- Resolution.py
+++ Resolution.py
@@ -29,12 +29,9 @@ t_COMMA = r","
 
 def t_ID(t):
     r'[a-zA-Z]\w*'
-    #print(t.value)#prints A,x,B,x
-    #print(t)        #Prints ID, A, ID, x
     return t
 
 def t_error(t):
-    #print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
 lexer = lex.lex()
@@ -46,7 +43,6 @@ def p_formula_binary(p):
                 | FORMULA AND FORMULA
                 """
     p[0] = [p[1],p[2],p[3]]#check if this is valid
-    #print(p[1][1],"P1")
 
 def p_formula_not(p):
     """FORMULA  : NOT FORMULA
@@ -57,7 +53,6 @@ def p_formula_not(p):
 def p_formula_group(p):
     "FORMULA : LPAREN FORMULA RPAREN"
     p[0] = p[2]
-    #print(p[0],"inside")
 
 def p_formula_ID(p):
     "FORMULA : TERM"
@@ -67,7 +62,6 @@ def p_term(p):
     """TERM : ID LPAREN TERMLIST RPAREN
             | ID"""
     p[0] = p[1] if len(p) == 2 else [p[1],p[3]]
-    #print (p[0],"Here")
 
 def p_termlist(p):
     """TERMLIST : TERM COMMA TERMLIST
@@ -76,10 +70,8 @@ def p_termlist(p):
         p[0] = p[1]
 
     else:
-        #print(p[1],"and",p[3])
         p[0] = [p[1],p[3]]
     #p[0] = p[1] if len(p) == 2 else [p[1]] + [p[3]]
-    #print(p[0],"here now",print(len(p),"LLength"))
 
 precedence = (
 ("right", "IMPLIES"),
@@ -99,7 +91,6 @@ parser = yacc.yacc()
 #***check impliaction exists**
 
 def implication_exists(a):
-    #print(a,"this is a",len(a))
     if len(a)==3:
         if "=>" in a:
             return True
@@ -167,7 +158,7 @@ def implication_removal(logic):
         elif len(logic[1])==2:
             if logic[1][0] == "~":
                 logic = logic[1][1]
-    #print(logic,"PPPPPPPPPP")
+
     if len(logic)== 2 and logic[0]=="~":
         if len(logic[1])==2 and logic[1][0]=="~":
             logic = implication_removal(logic)
@@ -212,14 +203,9 @@ def implication_removal(logic):
             d.append(last_term)
             logic = d
 
-    #print(logic,"after iteration")
-    #print(len(logic[2]))
-    #print(logic[2])
-
     for j in range(len(logic)):
 
         if len(logic[j])>1:
-            #print(logic[j],"logicj",len(logic[j]))
             logic[j] = implication_removal(logic[j])
     return logic
 
@@ -232,10 +218,9 @@ def OrAnd_Check(a):
 
 
 def distribution_Or_And(logic):
-    #print("here")
+
     if len(logic)<3:
         pass
-        #print(logic, "this was printed")
     elif len(logic) == 3:
         if OrAnd_Check(logic):
             if len(logic[0])!=3 and len(logic[2])==3:
@@ -291,7 +276,6 @@ def distribution_Or_And(logic):
 
     for j in range(len(logic)):
         if len(logic[j])>1:
-            #print(logic[j],"logicj",len(logic[j]))
             logic[j] = distribution_Or_And(logic[j])
 
     if OrAnd_Check(logic):
@@ -300,11 +284,8 @@ def distribution_Or_And(logic):
     return logic
 
 def flattening(logic):
-    #print(logic, "Length", len(logic))
     if len(logic)==2:
         if logic[0]== "~":
-            #print(logic[1])
-            #print(logic[1][1],"has not")
             s=[]
             if len(logic[1][1])>2:
                 logic[1][1] = [logic[1][1]]
@@ -318,11 +299,9 @@ def flattening(logic):
                     logic[1][1]=[logic[1][1]]
             else:
                 m = flatten(logic[1][1], s)
-                #print(m, "flat not predicate")
                 logic[1][1] = m
 
         else:
-            #print(logic[1],"no not",len(logic[1]),logic[1][1])
             s=[]
             if len(logic[1])>2:
                 logic[1] = [logic[1]]
@@ -330,14 +309,12 @@ def flattening(logic):
             elif len(logic[1])==2:
                 if type(logic[1])==list:
                     m = flatten(logic[1], s)
-                    #print(m)
                     logic[1] = m
                 else:
                     logic[1]=[logic[1]]
 
             else:
                 m = flatten(logic[1], s)
-                #print(m, "flat no not predicate")
                 logic[1] = m
 
     if len(logic)==3:
@@ -369,12 +346,10 @@ def variable_generator(variables):
 def standardiztion(logic):
     if len(logic)==2:
         if logic[0]== "~":
-            #print(logic[1][1],"PPPP")
             n = variable_generator(logic[1][1])
             logic[1][1]=n
 
         else:
-            #print(logic[1],"QQQQ",len(logic[1]))
             n = variable_generator(logic[1])
             logic[1]=n
 
@@ -423,7 +398,6 @@ def cleanUp(logic):
 
     for j in range(len(logic)):
         if len(logic[j])>1:
-            #print(logic[j],"logicj",len(logic[j]))
             logic[j] = cleanUp(logic[j])
 
 
@@ -514,8 +488,6 @@ def substitution(clause,out_theta):
 
 
 def resolution(clause_1,clause_2,pr_chk,quer_pred):
-
-    #print(clause_1,"c1 b4 resolution",len(clause_1),'\n',clause_2,"c2 b4 res",len(clause_2))
     if len(clause_2)<=2:        #single predicate
         clause_2=[]
     elif len(clause_2)>2:
@@ -582,18 +554,14 @@ def resolution(clause_1,clause_2,pr_chk,quer_pred):
                             clause_1.pop(pos_5)
                             clause_1.pop(pos_5 - 1)
                             break
-    #print(clause_1,"c1-inside resolution")
-    #print(clause_2,"c-2-inside resolution")
     if clause_1 ==[] or clause_2 == []:
         resolved_clause = clause_2+clause_1
     else:
         resolved_clause = clause_2+["|"]+clause_1
-    #print(resolved_clause)
     return resolved_clause
 
 
 def main_resolution(clause):
-    #print(clause)
     x=0
     querry_predicate=0
     clause_2 = copy.deepcopy(clause)
@@ -617,7 +585,6 @@ def main_resolution(clause):
             pred_check = each_term[0]
             x = each_term[1]
 
-    #print(pred_check,querry_predicate,"pred check & queery")
     if type(x) == list and len(x) == 1:
         x = x[0]
     y=0
@@ -650,41 +617,28 @@ def main_resolution(clause):
 
         if type(y) == list and len(y) == 1:
             y = y[0]
-        #print(x,y)
         out = unify(x, y, theta=[])
-        #print(out,"out")
         if out!= failure:
             resolved = resolution(clause_1,clause_2,pred_check,querry_predicate)
-            #print(resolved,"resolved")
             if resolved == []:
-                #print("Success")
                 return  "TRUE"
             op = substitution(resolved,out)
             if op not in explored_rules:
                 explored_rules.append(op)
-
-                #print(op)
                 if len(op)>2:
                     queue.append(op)
                 else:
                     queue=queue+op
-
-
-    #print(queue,"FQ")  # unified statements obtained
 
     if [] in queue:
         return "TRUE"
     else:
         for new_items in queue:
             new_clause = new_items
-            #print(new_clause,"new clause")
             k = main_resolution(new_clause)
             if k == "TRUE":
                 k="TRUE"
                 return k
-
-
-
 
 #********MAIN************
 in_file = open("input.txt","r")
@@ -694,15 +648,10 @@ queries = []
 rules = []
 
 for i in range(no_queries):
-    #queries.append(in_file.readline().split('\n')[0])
-    #print(queries)
     d=in_file.readline().split('\n')[0]
     question = parser.parse(str(d))
-    #print(question)
     question_after_flattening = flattening(question)
-    #print(question_after_flattening,"flat")
     question_after_standardization = standardiztion(question_after_flattening)
-    #print(question_after_standardization,"stand")
     queries.append(question_after_standardization)
 
 no_rules = int(in_file.readline().split('\n')[0])
@@ -710,50 +659,22 @@ no_rules = int(in_file.readline().split('\n')[0])
 for j in range(no_rules):
 
     c=in_file.readline().split('\n')[0]
-    #print(c)
-    #print(rules)
-    #print(len(rules[0]))
-
     res = parser.parse(str(c)) #((A=>B)|C) ((A(X) => B(X))|C(X))
     rules.append(res)
-    #print(res)
-
     res_after_implication_removal = implication_removal(res)
-    #print(res_after_implication_removal,"after removal")
-        #print(explored,"explored set")
-
     res_after_flattening = flattening(res_after_implication_removal)
-        #print(explored,"till now")
-    #print(res_after_flattening,"after flat")
-    #print(len(res_after_flattening))
     res_after_standardization = standardiztion(res_after_flattening)
-        #for varrs in session_explored:
-            #explored.append(varrs)
     explored = explored + session_explored
     session_explored=[]
-    #print(res_after_standardization,"after standardization")
     res_after_or_over_and = distribution_Or_And(res_after_standardization)
-    #print(res_after_or_over_and,"after OR and and dist")
-        #print(res_after_or_over_and[2],len(res_after_or_over_and),res_after_or_over_and[2][1])
     res_after_clean_up = cleanUp(res_after_or_over_and)
-    #print(res_after_clean_up,"clean up",len(res_after_clean_up))
-
     if "&" in res_after_clean_up:
         for each_element in res_after_clean_up:
             if each_element != "&":
-                #print(each_element)
                 kb.append(each_element)
     else:
         kb.append(res_after_clean_up)
 
-    #kb.append(res_after_clean_up)
-#if "&" in kb:              #check for split
-    #print("should not print")
-#print(kb,len(kb))
-
-"""for el_10 in kb:
-    out_file.write(str(el_10))
-    out_file.write('\n')"""
 for individual_rule in kb:
     #out_file.write(str(individual_rule))
     #out_file.write('\n')
@@ -781,23 +702,13 @@ for individual_rule in kb:
                     else:
                         hash_table[predicate_to_sort]=hash_table[predicate_to_sort]+[individual_rule]
 
-#print(hash_table)
-#out_file.write('\n')
-#out_file.write('\n')
-#out_file.write(str(hash_table))
-#print(hash_table["R"],"print?",len(hash_table["R"]))
-
-
 #********* TO CHECK unification*****
 
 #***select query 1 and negate it, find the predicate for query**
 
-#CLAUSE_2 == query ; Clause1 = rule
-for clause_2 in queries:     ##**** TODO update to pop***
-    #print(clause_2,"before negation")
+for clause_2 in queries:
     if clause_2[0]=="~":
         clause_2=clause_2[1]
-        #querry_predicate =clause_2[0]
 
     else:
         h=[]
@@ -806,126 +717,12 @@ for clause_2 in queries:     ##**** TODO update to pop***
         h.append(F_term)
         h.append(S_term)
         clause_2=h
-        #querry_predicate=clause_2[1][0]
 
-    #print(clause_2,"after negation")
     res_after_main_resolution = main_resolution(clause_2)
     if res_after_main_resolution != "TRUE":
         res_after_main_resolution = "FALSE"
 
     out_file.write(str(res_after_main_resolution)+'\n')
-    #print(res_after_main_resolution)
-
-
-
-    #frontier=hash_table[predicate_for_frontier]
-
-    #print(frontier,"frontier",len(frontier))
-    #print(hash_table["~A"])
-
-
-    """while(len(frontier_queue))!=0:
-        clause_2=frontier_queue.pop(0)
-        if len(clause_2)<=2:
-            if clause_2[0] == "~":
-                predicate_for_frontier = clause_2[1][0]
-                x = clause_2[1][1]
-            else:
-                predicate_for_frontier = "~" + clause_2[0]
-                x= clause_2[1]
-        else:
-            for expr in clause_2:
-                if expr[0]=="~":
-                    predicate_for_frontier=expr[1][0]
-                    x= expr[1][1]
-                    break
-                else:
-                    predicate_for_frontier = "~"+expr[0]
-                    x=expr[1]
-                    break
-
-        frontier = hash_table[predicate_for_frontier]
-        print(clause_2,"b4 loop")
-        print(frontier,"b4 loop")
-        print(explored_rules,"expored rules")
-        querry_predicate=predicate_for_frontier
-        new_frontier_queue=[]
-        for clauses in frontier:
-            clause_2_to_work = copy.deepcopy(clause_2)
-
-            clause_1 = clauses
-            #clause_1 = frontier.pop(0)
-
-            # print(clause_1,len(clause_1))
-            # print(x)
-            if clause_1 not in explored_rules:
-
-                if len(clause_1) == 2:
-                    if clause_1[0] == "~":
-                        y = clause_1[1][1]
-                    else:
-                        y = clause_1[1]
-
-                else:
-                    for terms in clause_1:
-                        if len(terms) > 1:
-                            if terms[0] == "~":
-                                check = terms[1][0]
-                                arg = terms[1][1]
-                                if check == querry_predicate:
-                                    y = arg
-                            else:
-                                check = terms[0]
-                                arg = terms[1]
-
-                                if check == querry_predicate:
-                                    y = arg
-
-                if type(x) == list and len(x) == 1:
-                    x = x[0]
-
-                if type(y) == list and len(y) == 1:
-                    y = y[0]
-
-                #print(len(x), len(y))
-                print(x, y,"x and y")
-                out = unify(x, y, theta)
-
-                print(clause_1, "c1")
-                print(clause_2, "c2")
-                print(out,"sub to take place")
-                # print(len(clause_1))
-
-
-                if out != failure:
-
-                    resolved = resolution(clause_1, clause_2_to_work, querry_predicate)
-                    print(resolved, "resolved", len(resolved))
-                    op = substitution(resolved)
-                    print(op,"after subs")
-                    clause_2_explored = copy.deepcopy(op)
-                    explored_rules.append(clause_2_explored)
-                    theta=[]
-                    # clause=[clause_2]+[clause_1]
-                    # print(clause)
-                     #obtained resolved clause
-
-
-                    if resolved == []:
-                        print("TRUE")
-                        frontier = []
-                        print("YAY")
-                        frontier_queue = []
-
-                    else:
-                        if len(resolved)==1:
-                            new_frontier_queue.append(resolved[0])
-                        else:
-                            new_frontier_queue.append(resolved)
-
-        frontier_queue = new_frontier_queue+frontier_queue
-        print(frontier_queue,"FQ")"""
-
 
 #*****CNF Conversion******
 
